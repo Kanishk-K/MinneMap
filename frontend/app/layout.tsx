00ef78fc6faaa075d86ccdae3413bb61af2c8c10
Kanishk-K <kanishkkacholia@gmail.com>
2025-05-09 21:07:24 -0500

    🔭 Navbar setup

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Layout/Navbar";
+import { ThemeProvider } from "@/components/Layout/ThemeProvider";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-geist-font",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -23,11 +21,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+        <main>
+          <Navbar />
+          {children}
+        </main>
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ frontend/components/Layout/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <div>MinneMap</div>
+            <div>Hello 2</div>
+            <div>Hello 3</div>
+        </nav>
+    )
+}
