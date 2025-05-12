90a0dd9bd1b43193a9c351b28c23263339cebf81
Kanishk-K <kanishkkacholia@gmail.com>
2025-05-11 19:39:25 -0500

    ⏹ Square selection completed.

--- frontend/components/Widgets/ImageEditor.tsx
+++ frontend/components/Widgets/ImageEditor.tsx
@@ -1,4 +1,5 @@
 "use client"
+import 'react-image-crop/dist/ReactCrop.css'
 
 import React from "react"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
@@ -6,6 +7,7 @@ import { Undo, Upload } from "lucide-react"
 import { Input } from "../ui/input"
 import { Progress } from "../ui/progress"
 import { Button } from "../ui/button"
+import ReactCrop, { type Crop } from "react-image-crop"
 
 /*
 Handles the following steps:
@@ -15,20 +17,54 @@ Handles the following steps:
 4. Request additional information about the event.
 */
 
+const steps: Record<number, {title:string, description:string}> = {
+    0: {
+        title: "Upload a File",
+        description: "Upload the media you intend to use for promoting your event."
+    },
+    1: {
+        title: "Select a Square Area",
+        description: "Select a square area in the image for a QR code location."
+    },
+    2: {
+        title: "Approve QR Code Location",
+        description: "Put a dummy QR code in the selected area, allow the user to approve."
+    },
+    3: {
+        title: "Request Additional Information",
+        description: "Request additional information about the event."
+    }
+}
+
 export default function ImageEditor() {
     const [file, setFile] = React.useState<File | null>(null)
+    const [crop, setCrop] = React.useState<Crop>()
     const [progress, setProgress] = React.useState(0)
 
     function proceed(){
         if (progress == 0) {
             return file !== null
         }
+        if (progress == 1) {
+            return crop !== undefined && crop.width > 0 && crop.height > 0
+        }
     }
 
     function goBack(){
         if (progress == 1) {
             setFile(null)
+            setCrop(undefined)
             setProgress(0)
+        }
+        if (progress == 2) {
+            setCrop(undefined)
+            setProgress(1)
+        }
+    }
+
+    function goNext(){
+        if (progress == 1 && crop !== undefined && crop.width > 0 && crop.height > 0) {
+            setProgress(2)
         }
     }
 
@@ -47,8 +83,8 @@ export default function ImageEditor() {
     return (
         <Card className="col-span-2 sm:col-span-4 md:col-span-6">
             <CardHeader>
-                    <CardTitle>Upload a File</CardTitle>
-                    <CardDescription>Upload the media you intend to use for promoting your event.</CardDescription>
+                    <CardTitle>{steps[progress].title}</CardTitle>
+                <CardDescription>{steps[progress].description}</CardDescription>
             </CardHeader>
             <CardContent>
                 {/* Step 1 */}
@@ -67,14 +103,16 @@ export default function ImageEditor() {
                     </div>
                 </form>}
                 {/* Step 2 */}
-                {progress == 1 && (<div className="flex flex-col items-center justify-center w-full min-h-64 border-2 border-muted-foreground border-dashed rounded-lg bg-background">
-
+                {progress == 1 && (<div className="flex flex-col p-8 items-center justify-center w-full min-h-64 border-2 border-muted-foreground border-dashed rounded-lg bg-background">
+                    <ReactCrop crop={crop} onChange={(c) => setCrop(c)} aspect={1}>
+                        <img src={URL.createObjectURL(file!)} alt="Preview" className="rounded-lg" />
+                    </ReactCrop>
                 </div>)}
             </CardContent>
             <CardFooter className="gap-4">
                 <Progress value={progress * 25} className="w-full" />
                 <Button variant="outline" className="cursor-pointer" disabled={progress==0} onClick={goBack}><Undo />Back</Button>
-                <Button variant="outline" className="cursor-pointer" disabled={!proceed()}>Next</Button>
+                <Button variant="outline" className="cursor-pointer" disabled={!proceed()} onClick={goNext}>Next</Button>
             </CardFooter>
         </Card>
     )
---
+++ frontend/components/Widgets/Tables/Campaign/CampaignColumns.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { DataTable } from "./CampaignTable"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut } from "@/components/ui/dropdown-menu"
+import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { Eye, Map, MoreHorizontal, Trash2, UserRoundPlus } from "lucide-react"
+import { Button } from "@/components/ui/button"
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Campaign = {
+  id: string
+  date: string
+  name: string
+  views: number
+}
+
+export const columns: ColumnDef<Campaign>[] = [
+  {
+    accessorKey: "date",
+    header: "Date",
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("date"))
+      return date.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      })
+    }
+  },
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ row }) => {
+      const name:string = row.getValue("name")
+      return name.length > 30 ? `${name.slice(0, 30)} ...` : name
+    }
+  },
+  {
+    accessorKey: "views",
+    header: "Views",
+    cell: ({ row }) => {
+      const views:number = row.getValue("views")
+      return views.toLocaleString("en-US")
+    }
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const campaign = row.original
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Campaign Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              View campaign
+              <DropdownMenuShortcut><Eye /></DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Share campaign
+              <DropdownMenuShortcut><UserRoundPlus /></DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Campaign Map
+              <DropdownMenuShortcut><Map /></DropdownMenuShortcut>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem onClick={() => {console.log("Delete campaign", campaign.id)}}>
+              Delete Campaign
+              <DropdownMenuShortcut><Trash2 className="dark:stroke-red-400 stroke-red-600"/></DropdownMenuShortcut>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    }
+  }
+]
+
+export default function CampaignTable({ data }: { data: Campaign[] }) {
+    return (
+        <DataTable columns={columns} data={data} className="col-span-2 sm:col-span-4 md:col-span-4 lg:col-span-4 row-span-2" />
+    )
+}
